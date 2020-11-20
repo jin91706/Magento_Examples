e52f90bbb4c315d4b55a1efc06272d0f0bed9db7
James Sedillo <jin91706@yahoo.com>
2020-11-20 07:00:41 -0800

    Initial commit

---
+++ app/code/JamesSedillo/JSForm/Model/ResourceModel/Jsform.php
@@ -0,0 +1,17 @@
+<?php
+namespace JamesSedillo\JSForm\Model\ResourceModel;
+
+class Jsform extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+    public function __construct(
+        \Magento\Framework\Model\ResourceModel\Db\Context $context
+    )
+    {
+        parent::__construct($context);
+    }
+
+    protected function _construct()
+    {
+        $this->_init('jsform', 'id');
+    }
+}
